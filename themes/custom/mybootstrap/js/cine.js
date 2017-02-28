7dcea4cf512a27e74e025db8d3d46256c5ba6901
hugo <hugo.lopez.90@gmail.com>
2017-02-28 13:23:51 +0100

    first

--- themes/custom/mybootstrap/js/cine.js
+++ themes/custom/mybootstrap/js/cine.js
@@ -1,20 +1,13 @@
 (function($,Drupal, drupalSettings){
 
     //plier cacher
-
     Drupal.behaviors.collapsableBlock = {
         attach: function(context, settings){
+
             $('.panel-heading', context).click(function(){
                 $(this).parent().find('.collapse').slideToggle('fast');
             });
         }
     }
 
-//initialisation de match height
-
-    /*	Drupal.behaviors.collapsableBlock = {
-     attach: function(context, settings){
-     $('.views-col').matchHeight();
-     }
-     }*/
 })(jQuery, Drupal, drupalSettings);
